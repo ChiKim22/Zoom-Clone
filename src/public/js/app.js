47ca17331896ce120ab21939558bbb970ea7a8e5
ChiKim22 <wlsgh2505@gmail.com>
2022-03-10 12:43:37 +0900

    Zoom Clone(connect WebSocket)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,37 @@
-console.log("hi");
+// console.log("hi");
+
+const msgList = document.querySelector("ul");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+    console.log("U connected!!!!!!");
+});
+
+socket.addEventListener('message', (msg) => {
+    // console.log("just get this!!!!", msg, "from to server");
+    console.log("just get this!!!!", msg.data, "from to server");
+});
+
+socket.addEventListener('close', () => {
+    console.log("U has been Disconncted...");
+});
+
+// setTimeout(func, 3000); //3초 후에 실행.
+// socket.send("Hello from the browser...");
+
+const msgForm = document.querySelector("form");
+
+
+function handleSubmit(event) {
+    event.preventDefault(); // 서버로 요청 보내는 것을 막아줌.
+    // form 태그 내의 input 태그에 입력된 값을 읽는다.
+    const input = msgForm.querySelector("input");
+
+    // 그 값을 socket를 통해 WebSocket Sever로 전송.
+    console.log("Submit event occurred...", input.value);
+    socket.send(input.value);
+    input.value = "";
+}
+
+msgForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,48 @@
 import express from "express";
-
+import http from "http";
+import WebSocket from "ws";
+import { homedir } from "os";
 const app = express();
 
-console.log('Hello world!');
+// console.log('Hello world!');
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views"); 
 app.use("/public", express.static(__dirname + "/public"));
 
-function handleReq(req, res){
-    res.render("home");
-}
-app.get("/", handleReq);
+// function handleReq(req, res){
+//     res.render("home");
+// }
 
-app.listen(3333);
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3333`);
+//  node.js 에 이미 포함되어 있는 http 패키지 사용.
+const server = http.createServer(app);
+
+//  server 파라미터는 optional, Websocket 서버와 http 서버를 함께 생성하고자 할 때 사용.
+// http 서버이며, WebSocket 서버인 프로레스가 3333 포트에 바인딩되어 실행됨.
+
+const wss = new WebSocket.Server({server});
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", (socket) => {
+    console.log("Successfully connected");
+    socket.on('close', () => {
+        console.log("clossed...");
+    });
+
+    socket.on("message", (msg) => {
+        console.log(Buffer.from(msg, "base64").toString("utf-8"));
+        socket.send(Buffer.from(msg, "base64").toString("utf-8"));
+    });
+        // socket.send("Hello World!!!");
+});
+
+// app.listen(3333);
+// websocket 서버 생성
+server.listen(3333, handleConnection);
